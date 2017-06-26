7b4316eb326aa882f7defd4a8d6ae537ba2e26fe
xwang <xunwang@wenba100.com>
2017-06-26 15:47:32 +0800

    crash handler

--- app/src/main/java/setupandroidcomm/xun/com/setupandroidcomm/log/CommLogHandler.java
+++ app/src/main/java/setupandroidcomm/xun/com/setupandroidcomm/log/CommLogHandler.java
@@ -6,26 +6,19 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Environment;
 import android.os.Looper;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.lang.reflect.Field;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import setupandroidcomm.xun.com.setupandroidcomm.utils.CacheStoreUtil;
@@ -38,19 +31,9 @@ public class CommLogHandler implements Thread.UncaughtExceptionHandler {
     //Log Tag
     public static final String TAG = "CommLogHandler";
     //异常信息
-    public static final String EXCEPETION_INFOS_STRING = "EXCEPETION_INFOS_STRING";
-    //应用包信息
-    public static final String PACKAGE_INFOS_MAP = "PACKAGE_INFOS_MAP";
-    //设备数据信息
-    public static final String BUILD_INFOS_MAP = "BUILD_INFOS_MAP";
-    //系统常规配置信息
-    public static final String SYSTEM_INFOS_MAP = "SYSTEM_INFOS_MAP";
-    //手机安全配置信息
-    public static final String SECURE_INFOS_MAP = "SECURE_INFOS_MAP";
-    //内存情况信息
-    public static final String MEMORY_INFOS_STRING = "MEMORY_INFOS_STRING";
-    public static final String VERSION_NAME = "versionName";
-    public static final String VERSION_CODE = "versionCode";
+    public static final String EXCEPETION_INFOS_STRING = "excepetion_infos_string";
+    //基本信息
+    public static final String APP_INFOS_STRING = "app_infos_string";
 
     private static volatile CommLogHandler INSTANCE;
     private static Context mContext;
@@ -58,11 +41,8 @@ public class CommLogHandler implements Thread.UncaughtExceptionHandler {
 
     //用来存储设备信息和异常信息
     private ConcurrentHashMap<String, Object> infos = new ConcurrentHashMap<>();
-    private ConcurrentHashMap<String, String> mPackageInfos = new ConcurrentHashMap<>();
-    private ConcurrentHashMap<String, String> mDeviceInfos = new ConcurrentHashMap<>();
-    private String mMemInfos = new String();
-    private ConcurrentHashMap<String, String> mSystemInfos = new ConcurrentHashMap<>();
-    private ConcurrentHashMap<String, String> mSecureInfos = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, String> appInfoMap = new ConcurrentHashMap<>();
+    private StringBuffer AppInfoSB = new StringBuffer();
 
     //用于格式化日期,作为日志文件名的一部分
     private DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
@@ -135,11 +115,7 @@ public class CommLogHandler implements Thread.UncaughtExceptionHandler {
         String mTime = formatter.format(new Date());
         StringBuffer mStringBuffer = new StringBuffer();
         mStringBuffer.append("--start--------------").append(mTime).append("----------------").append("\r\n");
-        mStringBuffer.append(getInfosStr("PackageInfos --> ", mPackageInfos));
-//        mStringBuffer.append(getInfosStr("DeviceInfos --> ", mDeviceInfos));
-//        mStringBuffer.append(getInfosStr("SystemInfos --> ", mSystemInfos));
-//        mStringBuffer.append(getInfosStr("SecureInfos --> ", mSecureInfos));
-//        mStringBuffer.append("MemInfos --> ").append(mMemInfos).append("\r\n");
+        mStringBuffer.append(AppInfoSB.toString());
         mStringBuffer.append(mExceptionInfos);
         mStringBuffer.append("--end--------------").append(mTime).append("----------------").append("\r\n");
         // 保存文件，设置文件名
@@ -161,21 +137,6 @@ public class CommLogHandler implements Thread.UncaughtExceptionHandler {
             }
         }
         return null;
-    }
-
-    /**
-     * 将HashMap遍历转换成StringBuffer
-     */
-    @NonNull
-    public static StringBuffer getInfosStr(String tips, ConcurrentHashMap<String, String> infos) {
-        StringBuffer mStringBuffer = new StringBuffer();
-        mStringBuffer.append(tips).append(":");
-        for (Map.Entry<String, String> entry : infos.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            mStringBuffer.append(key + "=" + value + "\r\n");
-        }
-        return mStringBuffer;
     }
 
     /**
@@ -250,115 +211,35 @@ public class CommLogHandler implements Thread.UncaughtExceptionHandler {
             // 获得包管理器
             PackageManager mPackageManager = context.getPackageManager();
             // 得到该应用的信息，即主Activity
-            PackageInfo mPackageInfo = mPackageManager.getPackageInfo(context.getPackageName(), PackageManager.GET_ACTIVITIES);
+            PackageInfo mPackageInfo = mPackageManager.getPackageInfo(context.getPackageName(), PackageManager
+                    .GET_ACTIVITIES);
             if (mPackageInfo != null) {
                 String versionName = mPackageInfo.versionName == null ? "null" : mPackageInfo.versionName;
                 String versionCode = mPackageInfo.versionCode + "";
-                mPackageInfos.put(VERSION_NAME, versionName);
-                mPackageInfos.put(VERSION_CODE, versionCode);
+                AppInfoSB.append("VersionName: ").append(versionName).append("\n").append("VersionCode: ").append
+                        (versionCode).append("\n");
             }
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * 从系统属性中提取设备硬件和版本信息
-     */
-    private void collectBuildInfos() {
-        // 反射机制
-        Field[] mFields = Build.class.getDeclaredFields();
-        // 迭代Build的字段key-value 此处的信息主要是为了在服务器端手机各种版本手机报错的原因
-        for (Field field : mFields) {
-            try {
-                field.setAccessible(true);
-                mDeviceInfos.put(field.getName(), field.get("").toString());
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
+    private void dumpPhoneInfo() {
+        //android版本号
+        AppInfoSB.append("Android OS Version: ").append(Build.VERSION.RELEASE).append("_").append(Build.VERSION
+                .SDK_INT).append("\n");
+
+        //手机制造商
+        AppInfoSB.append("Vendor: ").append(Build.MANUFACTURER).append("\n");
+
+        //手机型号
+        AppInfoSB.append("Model: ").append(Build.MODEL).append("\n");
+
+        //cpu架构
+        AppInfoSB.append("CPU ABI: ").append(Build.CPU_ABI).append("\n");
+        appInfoMap.put(APP_INFOS_STRING, AppInfoSB.toString());
     }
 
-    /**
-     * 获取系统常规设定属性
-     */
-    private void collectSystemInfos() {
-        Field[] fields = Settings.System.class.getFields();
-        for (Field field : fields) {
-            if (!field.isAnnotationPresent(Deprecated.class)
-                    && field.getType() == String.class) {
-                try {
-                    String value = Settings.System.getString(mContext.getContentResolver(), (String) field.get(null));
-                    if (value != null) {
-                        mSystemInfos.put(field.getName(), value);
-                    }
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    /**
-     * 获取系统安全设置信息
-     */
-    private void collectSecureInfos() {
-        Field[] fields = Settings.Secure.class.getFields();
-        for (Field field : fields) {
-            if (!field.isAnnotationPresent(Deprecated.class)
-                    && field.getType() == String.class
-                    && field.getName().startsWith("WIFI_AP")) {
-                try {
-                    String value = Settings.Secure.getString(mContext.getContentResolver(), (String) field.get(null));
-                    if (value != null) {
-                        mSecureInfos.put(field.getName(), value);
-                    }
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    /**
-     * 获取内存信息
-     */
-    private String collectMemInfos() {
-        BufferedReader br = null;
-        StringBuffer sb = new StringBuffer();
-
-        ArrayList<String> commandLine = new ArrayList<>();
-        commandLine.add("dumpsys");
-        commandLine.add("meminfo");
-        commandLine.add(Integer.toString(android.os.Process.myPid()));
-        try {
-            Process process = Runtime.getRuntime()
-                    .exec(commandLine.toArray(new String[commandLine.size()]));
-            br = new BufferedReader(new InputStreamReader(process.getInputStream()), 8192);
-
-            while (true) {
-                String line = br.readLine();
-                if (line == null) {
-                    break;
-                }
-                sb.append(line);
-                sb.append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return sb.toString();
-    }
 
     /**
      * 获取设备参数信息
@@ -368,18 +249,11 @@ public class CommLogHandler implements Thread.UncaughtExceptionHandler {
     private void collectInfos(Context context, Throwable ex) {
         mExceptionInfos = collectExceptionInfos(ex);
         collectPackageInfos(context);
-        collectBuildInfos();
-        collectSystemInfos();
-        collectSecureInfos();
-        mMemInfos = collectMemInfos();
+        dumpPhoneInfo();
 
         //将信息储存到一个总的Map中提供给上传动作回调
         infos.put(EXCEPETION_INFOS_STRING, mExceptionInfos);
-        infos.put(PACKAGE_INFOS_MAP, mPackageInfos);
-        infos.put(BUILD_INFOS_MAP, mDeviceInfos);
-        infos.put(SYSTEM_INFOS_MAP, mSystemInfos);
-        infos.put(SECURE_INFOS_MAP, mSecureInfos);
-        infos.put(MEMORY_INFOS_STRING, mMemInfos);
+        infos.put(APP_INFOS_STRING, appInfoMap);
     }
 
 }
